b20e4894771738f085dfb231bef8b66d38e7d42c
sachidanandapanigrahi <snpanigrahi88@gmail.com>
2022-03-11 10:53:48 +0530

    feat: add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -2,10 +2,18 @@
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
-module.exports = withPWA({
+const pwaConfig = withPWA({
   pwa: {
     dest: 'public',
     runtimeCaching,
     disable: process.env.NODE_ENV === 'development',
   },
 })
+
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+
+module.exports = withBundleAnalyzer(pwaConfig)
